950815c2f60d03b8c42ccb6b263ed516f295c361
Cesar Lage <kaerdsar@gmail.com>
2015-02-13 14:08:03 -0500

    add link to odoo saas, to use like cenit workspace

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -4,6 +4,17 @@ module ApplicationHelper
      uri = URI(url)
      uri.query = params.to_query
      uri.to_s
-   end
+  end
+
+  def open_workspace(user)
+    params = {
+        dbname: user.email.split('@').first,
+        login: 'admin',
+        name: user.name,
+        password: 'admin',
+        confirm_password: 'admin'
+    }
+    return generate_url("http://server:8069/saas_portal/signup", params)
+  end
    
 end

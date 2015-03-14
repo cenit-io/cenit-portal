d02e2088230a17c64d685e576cf3817029b051aa
Miguel Sancho <sanchocuba@gmail.com>
2015-03-14 11:09:49 -0400

    add validation to contact us form

--- app/controllers/contact_us/contacts_controller.rb
+++ app/controllers/contact_us/contacts_controller.rb
@@ -2,7 +2,7 @@ class ContactUs::ContactsController < ApplicationController
 
   def create
     @contact = ContactUs::Contact.new(params[:contact_us_contact])
-
+    byebug
     if @contact.save
       redirect_to('/', :notice => t('contact_us.notices.success'))
     else
@@ -13,7 +13,6 @@ class ContactUs::ContactsController < ApplicationController
 
   def new
     @contact = ContactUs::Contact.new
-    # @body_id = 'contact-us'
   end
 
 end

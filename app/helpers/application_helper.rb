c39d31b34f4325beb16cbb8b69dc898bd1b94396
Miguel Sancho <sanchocuba@gmail.com>
2015-04-18 19:34:09 -0400

    Created a new cenithub_url helper method similar to open_odoo helper method. Changed the name of open_odoo helper method and other tiny improves

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -6,30 +6,31 @@ module ApplicationHelper
      uri.to_s
   end
 
-  def open_workspace(user)
-    return unless (odoo_app = Doorkeeper::Application.where(name: 'odoo').first).present?
+  def cenitodoo_url(user, odoo_app)
     access_token = Doorkeeper::AccessToken.create!(
-        application_id: odoo_app.uid,
-        resource_owner_id: user.id,
-        scopes: 'userinfo',
-        expires_in: 7200
+      application_id: odoo_app.uid,
+      resource_owner_id: user.id,
+      scopes: 'userinfo',
+      expires_in: 7200
     )
     params = {
-        # db: user.email.split('@').first,
-        # login: user.email,
-        # client_id: odoo_app.uid,
-        # response_type: "code",
-        # redirect_uri: odoo_app.redirect_uri,
-        # scope: 'public',
-        access_token: access_token.token,
-        token_type: 'bearer',
-        expires_in: 7200,
-        state: {
-            login: user.email
-        }.to_json
+      access_token: access_token.token,
+      token_type: 'bearer',
+      expires_in: 7200,
+      state: {
+        login: user.email
+      }.to_json
     }
-    return generate_url("http://cenit-odoo.dev:8069/auth_oauth/doorkeeper_cb", params)
-    # return generate_url("/oauth/authorize", params)
+    redirect_url = odoo_app.redirect_uri.split.first
+    generate_url(redirect_url, params)
   end
-   
+  
+  def cenithub_url(cenit_app)
+    params = { 
+      client_id: cenit_app.uid, 
+      redirect_uri: cenit_app.redirect_uri.split.first, 
+      response_type: 'code'
+    }
+    generate_url("/oauth/authorize", params)
+  end
 end

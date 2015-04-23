5d50c542c91139114a5cc4c69ad29ea30a34a727
D.H. Bahr <dhbahr@gmail.com>
2015-04-23 16:44:16 -0400

    Demo support prototype

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -25,6 +25,26 @@ module ApplicationHelper
     generate_url(redirect_url, params)
   end
   
+  def cenitodoo_demo(user, odoo_app)
+    access_token = Doorkeeper::AccessToken.create!(
+      application_id: odoo_app.uid,
+      resource_owner_id: user.id,
+      scopes: 'userinfo',
+      expires_in: 7200
+    )
+    params = {
+      access_token: access_token.token,
+      token_type: 'bearer',
+      expires_in: 7200,
+      state: {
+        login: user.email,
+        demo: true
+      }.to_json
+    }
+    redirect_url = odoo_app.redirect_uri.split.first
+    generate_url(redirect_url, params)
+  end
+  
   def cenithub_url(cenit_app)
     params = { 
       client_id: cenit_app.uid, 

bb58d129ac1600ad99fba5b6455d578322611b88
D.H. Bahr <dhbahr@gmail.com>
2016-01-29 15:59:23 -0500

    Removied long corrected old behaviour

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -7,16 +7,7 @@ module ApplicationHelper
   end
 
   def cenitodoo_url(user, odoo_app)
-#    access_token = Doorkeeper::AccessToken.create!(
-#      application_id: odoo_app.uid,
-#      resource_owner_id: user.id,
-#      scopes: 'userinfo',
-#      expires_in: 7200
-#    )
     params = {
-#      access_token: access_token.token,
-#      token_type: 'bearer',
-#      expires_in: 7200,
       client_id: odoo_app.uid,
       redirect_uri: odoo_app.redirect_uri.split.first,
       response_type: 'token',
@@ -24,7 +15,6 @@ module ApplicationHelper
         login: user.email
       }.to_json
     }
-    # redirect_url = odoo_app.redirect_uri.split.first
     generate_url('/oauth/authorize', params)
   end
   

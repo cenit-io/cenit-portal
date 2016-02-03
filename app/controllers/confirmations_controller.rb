56b91536ee97577c5333f2ce1b02b7c3dc17b1cf
D.H. Bahr <dhbahr@gmail.com>
2016-02-03 15:23:42 -0500

    * Changed confirmation mail template * Account confirmation now signs user in

--- app/controllers/confirmations_controller.rb
+++ app/controllers/confirmations_controller.rb
@@ -1,5 +1,11 @@
 class ConfirmationsController < Devise::ConfirmationsController
 
+  def show
+    super do |resource|
+      sign_in(resource) if resource.errors.empty?
+    end
+  end
+
   private
 
   def after_confirmation_path_for(resource_name, resource)

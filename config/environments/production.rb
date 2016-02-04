46a676b97365608db30e63a2d59ac8e7d1f2a668
Miguel Sancho <sanchojaf@gmail.com>
2016-02-04 06:22:56 -0500

    added variable for domain

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -79,14 +79,16 @@ Cenit::Application.configure do
   # Use default logging formatter so that PID and timestamp are not suppressed.
   config.log_formatter = ::Logger::Formatter.new
   
-  config.action_mailer.default_url_options = {:host => 'cenitsaas.com'}
+  config.action_mailer.default_url_options = {:host => ENV['DOMAIN']}
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
     :address              => "smtp.gmail.com",
     :port                 => 587,
-    :domain               => 'cenitsaas.com',
+    :domain               => ENV['DOMAIN'],
     :user_name            => ENV['GMAIL_USERNAME'],
     :password             => ENV['GMAIL_PASSWORD'],
     :authentication       => 'plain',
     :enable_starttls_auto => true  }
+    
+    
 end

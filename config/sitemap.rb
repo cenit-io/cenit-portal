44490196fe1fd27246fec68e668c7d46f2b49c12
D.H. Bahr <dhbahr@gmail.com>
2016-05-12 11:45:38 -0400

    Updated hostnames

--- config/sitemap.rb
+++ config/sitemap.rb
@@ -1,5 +1,5 @@
 # Set the host name for URL creation
-SitemapGenerator::Sitemap.default_host = "https://www.cenitsaas.com"
+SitemapGenerator::Sitemap.default_host = "https://cenithub.com"
 
 SitemapGenerator.verbose = false
 
